af4a8d6561a7b62f1f86d02cc5c9c4363059d4b8
ZahirAndres <rodriguez.mora.zahir.15@gmail.com>
2025-02-24 10:14:14 -0600

    CarpetasV2

--- app/src/main/java/mx/edu/utng/tasking/UsuarioModelClass.kt
+++
@@ -1,9 +0,0 @@
-package mx.edu.utng.tasking
-
-data class UsuarioModelClass(
-    val userId: Int? = null,  // ID autogenerado
-    val userName: String,     // Nombre de usuario
-    val image : String?,
-    val userEmail: String,    // Correo electrónico
-    val password: String      // Contraseña del usuario
-)
